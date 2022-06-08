559d1cf51a152f2db630c9133ba3e8ce1da44641
MedsonF <medsonjosefrazao@outlook.com>
2022-06-07 21:45:04 -0300

    Definitivo

---
+++ 11721EAU015-ATV2/scr/main.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define STM32_RCC_BASE  0x40023800      // Reset and clock control RCC
+
+#define STM32_GPIOC_BASE    0x480000800U    //GPIOC
+
+#define STM32_RCC_AHB1ENR_OFFSET 0x0030  // AHB1 peripheral clock 
+
+#define STM32_GPIO_MODER_OFFSET 0x0000 // GPIO port mode register 
+#define STM32_GPIO_OTYPER_OFFSET 0x0004 // GPIO port output type register 
+#define STM32_GPIO_PUPDR_OFFSET 0x000c // GPIO port pull-up/pull-down
+#define STM32_GPIO_BSRR_OFFSET 0x0018 // GPIO reset register
+
+#define STM32_RCC_AHB1ENR (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
+#define STM32_GPIOC_MODER (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOC_OTYPER (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOC_PUPDR (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_BSRR (STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
+
+#define STM32_RCC_AHB1ENR_GPIOCEN       (1 << 2)  //bit 2 porta C clock
+
+        // GPIO MODE REGISTER
+
+#define STM32_GPIO_MODER_INPUT     (0)     // input pino GPIO
+#define STM32_GPIO_MODER_OUTPUT    (1)     // general output mode
+#define STM32_GPIO_MODER_ALT       (2)     // alternative mode
+#define STM32_GPIO_MODER_ANALOG    (3)     // analog mode
+
+#define STM32_GPIO_MODER13_SHIFT   (26)    
+#define STM32_GPIO_MODER13_MASK    (3 << STM32_GPIO_MODER13_SHIFT)
+
+        // GPIO OUTPUT REGISTER
+
+#define STM32_GPIO_OTYPER_PP       (0)     // output push-pull
+#define STM32_GPIO_OTYPER_OD       (1)     // output open drain
+
+#define STM32_GPIO_OT13_SHIFT      (13)
+#define STM32_GPIO_OT13_MASK       (1 << STM32_GPIO_OT13_SHIFT)
+
+        // GPIO PULL_UP/PULL_DOWN REGISTER
+
+#define STM32_GPIO_PUPDR_NONE       (0)     // no pull-up
+#define STM32_GPIO_PUPDR_PULLUP     (1)     // pull-up
+#define STM32_GPIO_PUPDR_PULLDOWN   (2)     //pull-down
+
+#define STM32_GPIO_PUPDR13_SHIFT    (26)    
+#define STM32_GPIO_PUPDR13_MASK     (3 << STM32_GPIO_PUPDR13_SHIFT)
+
+#define STM32_RCC_AHB1ENR_GPIOCEN       (1 << 2)  //bit 2 porta C clock
+
+#define GPIO_BSRR_SET(n) (1 << (n))
+#define GPIO_BSRR_RST(n) (1 << (n + 16))
+
+static uint32_t led_status;
+static const char     version [] = {'V', '1', '.', '0'};
+int main (int argc, char *argv[])
+{
+    uint32_t reg;
+
+    // Ponteiros para registradores 
+
+    uint32_t *pRCC_AHB1ENR = (uint32_t *)STM32_RCC_AHB1ENR;
+    uint32_t *pGPIOC_MODER = (uint32_t *)STM32_GPIOC_MODER;
+    uint32_t *pGPIOC_OTYPER = (uint32_t *)STM32_GPIOC_OTYPER;
+    uint32_t *pGPIOC_PUPDR = (uint32_t *)STM32_GPIOC_PUPDR;
+    uint32_t *pGPIOC_BSRR = (uint32_t *)STM32_GPIOC_BSRR;
+
+    // Habilita o clock no GPIOC
+
+    reg = *pRCC_AHB1ENR;
+    reg |= STM32_RCC_AHB1ENR_GPIOCEN;
+    *pRCC_AHB1ENR = reg;
+
+    // toda configuração da porta 13 como saida pull-up e pull-down off
+
+    reg = *pGPIOC_MODER;
+    reg &= ~(STM32_GPIO_MODER13_MASK);
+    reg |= (STM32_GPIO_MODER_OUTPUT << STM32_GPIO_MODER13_SHIFT);
+    *pGPIOC_MODER = reg;
+
+    reg = *pGPIOC_OTYPER;
+    reg &= ~(STM32_GPIO_OT13_MASK);
+    reg |= (STM32_GPIO_OTYPER_PP << STM32_GPIO_OT13_SHIFT);
+    *pGPIOC_OTYPER = reg;
+    
+    reg = *pGPIOC_PUPDR;
+    reg &= ~(STM32_GPIO_PUPDR13_MASK);
+    reg |= (STM32_GPIO_PUPDR_NONE << STM32_GPIO_PUPDR13_SHIFT);
+    *pGPIOC_PUPDR = reg;
+
+
+    while(1)
+    {
+        uint32_t LED_DELAY = 10;
+        *pGPIOC_BSRR = GPIO_BSRR_SET(13);
+        led_status = 0;
+        for (uint32_t i = 0; i < LED_DELAY; i++);
+        *pGPIOC_BSRR = GPIO_BSRR_RST(13);
+        led_status = 1;
+        for (uint32_t i = 0; i < LED_DELAY; i++);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ 11721EAU015-ATV2/scr/startup.c
@@ -0,0 +1,196 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define SRAM_START  0x20000000U                     // Inicio da SRAM CORTEX-M 
+#define SRAM_SIZE   (64U * 1024U)                   // Tam. SRAM STM32F401 64K 
+#define SRAM_END    ((SRAM_START) + (SRAM_SIZE))    // Final da SRAM STM32F401 
+
+#define STACK_START SRAM_END                        // Inicio da Stack 
+
+int main (void);
+
+void reset_handler      (void);
+void nmi_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void hardfault_handler  (void) __attribute__ ((weak, alias("default_handler")));
+void memmanage_handler  (void) __attribute__ ((weak, alias("default_handler")));
+void busfault_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void usagefault_handler (void) __attribute__ ((weak, alias("default_handler")));
+void svc_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void debugmon_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void pendsv_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void systick_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void wwdg_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void exti16_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void exti21_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void exti22_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void flash_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void rcc_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void exti0_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void exti1_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void exti2_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void exti3_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void exti4_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st0_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st1_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st2_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st3_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st4_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st5_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st6_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void adc_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void exti9_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void tim9_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void tim10_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void tim11_handler      (void) __attribute__ ((weak, alias("default_handler")));
+void tim1_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void tim2_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void tim3_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void tim4_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void i2c1_ev_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void i2c1_er_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void i2c2_ev_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void i2c2_er_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void spi1_handler       (void) __attribute__ ((weak, alias("default_handler")));           
+void spi2_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void usart1_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void usart2_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void exti15_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void exti17_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void exti18_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void dma1_st7_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void sdio_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void tim5_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void spi3_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st0_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st1_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st2_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st3_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st4_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void otg_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st5_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st6_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void dma2_st7_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void usart6_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void i2c3_ev_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void i2c3_er_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void fpu_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void spi4_handler       (void) __attribute__ ((weak, alias("default_handler")));
+
+extern uint32_t _sdata;                         // Inicio da secao .data 
+extern uint32_t _edata;                         // Fim da secao .data 
+extern uint32_t _la_data;                       // Endereco de carga na RAM da secao .data 
+
+extern uint32_t _sbss;                          // Inicio da secao .bss 
+extern uint32_t _ebss;                          // Fim da secao .bss 
+extern uint32_t _etext;                         // Fim da secao .text 
+
+uint32_t vectors[] __attribute__((section(".isr_vectors"))) =
+{
+    STACK_START,                                /* 0x0000 0000 */
+    (uint32_t)reset_handler,                    /* 0x0000 0004 */
+    (uint32_t)nmi_handler,                      /* 0x0000 0008 */
+    (uint32_t)hardfault_handler,                /* 0x0000 000c */
+    (uint32_t)memmanage_handler,                /* 0x0000 0010 */
+    (uint32_t)busfault_handler,                 /* 0x0000 0014 */
+    (uint32_t)usagefault_handler,               /* 0x0000 0018 */
+    0,                                          /* 0x0000 001c */
+    0,                                          /* 0x0000 0020 */
+    0,                                          /* 0x0000 0024 */
+    0,                                          /* 0x0000 0028 */
+    (uint32_t)svc_handler,                      /* 0x0000 002c */
+    (uint32_t)debugmon_handler,                 /* 0x0000 0030 */
+    0,                                          /* 0x0000 0034 */
+    (uint32_t)pendsv_handler,                   /* 0x0000 0038 */
+    (uint32_t)systick_handler,                  /* 0x0000 003c */
+    (uint32_t)wwdg_handler,                     /* 0x0000 0040 */
+    (uint32_t)exti16_handler,                   /* 0x0000 0044 */
+    (uint32_t)exti21_handler,                   /* 0x0000 0048 */
+    (uint32_t)exti22_handler,                   /* 0x0000 004c */
+    (uint32_t)flash_handler,                    /* 0x0000 0050 */
+    (uint32_t)rcc_handler,                      /* 0x0000 0054 */
+    (uint32_t)exti0_handler,                    /* 0x0000 0058 */
+    (uint32_t)exti1_handler,                    /* 0x0000 005c */
+    (uint32_t)exti2_handler,                    /* 0x0000 0060 */
+    (uint32_t)exti3_handler,                    /* 0x0000 0064 */
+    (uint32_t)exti4_handler,                    /* 0x0000 0068 */
+    (uint32_t)dma1_st0_handler,                 /* 0x0000 006c */
+    (uint32_t)dma1_st1_handler,                 /* 0x0000 0070 */
+    (uint32_t)dma1_st2_handler,                 /* 0x0000 0074 */
+    (uint32_t)dma1_st3_handler,                 /* 0x0000 0078 */
+    (uint32_t)dma1_st4_handler,                 /* 0x0000 007c */
+    (uint32_t)dma1_st5_handler,                 /* 0x0000 0080 */
+    (uint32_t)dma1_st6_handler,                 /* 0x0000 0084 */
+    (uint32_t)adc_handler,                      /* 0x0000 0088 */
+    (uint32_t)exti9_handler,                    /* 0x0000 009c */
+    (uint32_t)tim9_handler,                     /* 0x0000 00A0 */
+    (uint32_t)tim10_handler,                    /* 0x0000 00A4 */
+    (uint32_t)tim11_handler,                    /* 0x0000 00A8 */
+    (uint32_t)tim1_handler,                     /* 0x0000 00AC */
+    (uint32_t)tim2_handler,                     /* 0x0000 00B0 */
+    (uint32_t)tim3_handler,                     /* 0x0000 00B4 */
+    (uint32_t)tim4_handler,                     /* 0x0000 00B8 */
+    (uint32_t)i2c1_ev_handler,                  /* 0x0000 00BC */
+    (uint32_t)i2c1_er_handler,                  /* 0x0000 00C0 */
+    (uint32_t)i2c2_ev_handler,                  /* 0x0000 00C4 */
+    (uint32_t)i2c2_er_handler,                  /* 0x0000 00C8 */ 
+    (uint32_t)spi1_handler,                     /* 0x0000 00CC */                 
+    (uint32_t)spi2_handler,                     /* 0x0000 00D0 */
+    (uint32_t)usart1_handler,                   /* 0x0000 00D4 */
+    (uint32_t)usart2_handler,                   /* 0x0000 00D8 */
+    (uint32_t)exti15_handler,                   /* 0x0000 00E0 */
+    (uint32_t)exti17_handler,                   /* 0x0000 00E4 */
+    (uint32_t)exti18_handler,                   /* 0x0000 00E8 */
+    (uint32_t)dma1_st7_handler,                 /* 0x0000 00FC */
+    (uint32_t)sdio_handler,                     /* 0x0000 0104 */
+    (uint32_t)tim5_handler,                     /* 0x0000 0108 */
+    (uint32_t)spi3_handler,                     /* 0x0000 010C */
+    (uint32_t)dma2_st0_handler,                 /* 0x0000 0120 */
+    (uint32_t)dma2_st1_handler,                 /* 0x0000 0124 */
+    (uint32_t)dma2_st2_handler,                 /* 0x0000 0128 */
+    (uint32_t)dma2_st3_handler,                 /* 0x0000 012C */
+    (uint32_t)dma2_st4_handler,                 /* 0x0000 0130 */
+    (uint32_t)otg_handler,                      /* 0x0000 014C */
+    (uint32_t)dma2_st5_handler,                 /* 0x0000 0150 */
+    (uint32_t)dma2_st6_handler,                 /* 0x0000 0154 */
+    (uint32_t)dma2_st7_handler,                 /* 0x0000 0158 */
+    (uint32_t)usart6_handler,                   /* 0x0000 015C */
+    (uint32_t)i2c3_ev_handler,                  /* 0x0000 0160 */
+    (uint32_t)i2c3_er_handler,                  /* 0x0000 0164 */
+    (uint32_t)fpu_handler,                      /* 0x0000 0184 */
+    (uint32_t)spi4_handler,                     /* 0x0000 0190 */
+};
+
+void reset_handler()
+{
+    uint32_t i;
+    
+    // Copia a secao .data para a RAM 
+
+    uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
+    uint8_t *pDst = (uint8_t*)&_sdata;          // SRAM 
+    uint8_t *pSrc = (uint8_t*)&_etext;          // FLASH 
+
+    for(i =0; i < size; i++)
+    {
+        *pDst++ = *pSrc++;
+    }
+
+    // Preenche a secao .bss com zero 
+    
+    size = (uint32_t)&_ebss - (uint32_t)&_sbss;
+    pDst = (uint8_t*)&_sbss;
+
+    for(i =0; i < size; i++)
+    {
+        *pDst++ =0;
+    }
+
+    // Chama a funcao main() 
+
+    main();
+}
+void default_handler(void)
+{
+    while(1){};
+}
